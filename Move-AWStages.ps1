9b294a3cf4b0f841d5c33b341c78d9f84e2da39c
John Yoakum <jyoakum2013@gmail.com>
2025-03-18 03:35:32 -0800

    Add files via upload

--- Move-AWStages.ps1
+++ Move-AWStages.ps1
@@ -3,33 +3,75 @@ if (-not (Get-Module -ListAvailable -Name Liquit.Server.PowerShell)) {
     Install-Module -Name Liquit.Server.PowerShell -Scope CurrentUser -Force
 }
 
-$LiquitURI = 'https://zone.liquit.com' # Replace this with your zone
-$username = 'local\AccountName' # Replace this with a service account you have created for creating and accessing this information
-$password = 'PasswordForAccount' # Enter the password for that service Account
+$LiquitURI = 'https://liquit.corp.viamonstra.com' # Replace this with your zone
+$username = 'local\admin' # Replace this with a service account you have created for creating and accessing this information
+$password = 'Isaiah@2014' # Enter the password for that service Account
 $credentials = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username, (ConvertTo-SecureString -String $password -AsPlainText -Force)
 $daysBeforeAcceptance = 7
 $daysBeforeProduction = 7
+$PackageEvents = [System.Collections.ArrayList]::new()
+
 
 Connect-LiquitWorkspace -URI $LiquitURI -Credential $credentials
 
-$Packages = Get-LiquitPackage
+$AllPackages = Get-LiquitPackage
 
-ForEach ($Package in $Packages) {
-    $PackageSnapshots = Get-LiquitPackageSnapshot -Package $Package
-    ForEach ($PackageSnapshot in $PackageSnapshots) {
-        If ($PackageSnapshot.Type -eq "Test") {
-            $dateToCheck = Get-Date $($PackageSnapshot.ModifiedAt)
+ForEach ($Package in $AllPackages){
+    $EventData = Get-LiquitAuditingEvent -Entity $Package | Where-Object {$_.Type -eq 'RestAction'}
+    $TestPackageSnapshot = Get-LiquitPackageSnapshot -Package $Package -Type Test
+    $AcceptancePackageSnapshot = Get-LiquitPackageSnapshot -Package $Package -Type Acceptance
+    If ($TestPackageSnapshot) {
+        $EventData = Get-LiquitAuditingEvent -Entity $Package | Where-Object {$_.Type -eq 'RestAction' -and $_.Details.data.stage -eq 'Test'} | Sort-Object -Property CreatedAt -Descending | Select-Object -First 1
+        $EventDetails = New-Object PSObject -prop @{
+            PackageID = $Package.ID
+            PackageName = $Package.Name
+            PackageVersion = $EventData.Details.data.name
+            SnapshotID = $TestPackageSnapshot.ID
+            DateOfStage = $EventData.CreatedAt
+            SnapshotStage = 'Test'
+        }
+        [void]$PackageEvents.Add($EventDetails)
+    }
+    If ($AcceptancePackageSnapshot) {
+        $EventData = Get-LiquitAuditingEvent -Entity $Package | Where-Object {$_.Type -eq 'RestAction' -and $_.Details.data.stage -eq 'Acceptance'} | Sort-Object -Property CreatedAt -Descending | Select-Object -First 1
+        $EventDetails = New-Object PSObject -prop @{
+            PackageID = $Package.ID
+            PackageName = $Package.Name
+            PackageVersion = $EventData.Details.data.name
+            SnapshotID = $AcceptancePackageSnapshot.ID
+            DateOfStage = $EventData.CreatedAt
+            SnapshotStage = 'Acceptance'
+        }
+        [void]$PackageEvents.Add($EventDetails)
+    }
+}
+
+$Debug = $true
+# Now that I have the data, let's move it
+If (!$Debug) {
+    ForEach ($Event in $PackageEvents) {
+        If ($Event.SnapshotStage -eq 'Test') {
+            $CurrentPackage = Get-LiquitPackage -ID $Event.PackageID
+            $CurrentSnapshot = Get-LiquitPackageSnapshot -ID $Event.SnapshotID -Package $CurrentPackage
+            If ($Event.DateOfStage -ne $null) {
+                $dateToCheck = Get-Date $($Event.DateOfStage)
+            }
             $sevenDaysAgo = (Get-Date).AddDays(-$daysBeforeAcceptance)
-            if ($dateToCheck -lt $sevenDaysAgo) {
-                Publish-LiquitPackageSnapshot -Snapshot $PackageSnapshot -Stage Acceptance -Name $PackageSnapshot.Name -Description $PackageSnapshot.Description
+            If ($dateToCheck -lt $sevenDaysAgo -or $Event.DateOfStage -eq $null) {
+                Publish-LiquitPackageSnapshot -Snapshot $CurrentSnapshot -Stage Acceptance -Name $Event.PackageVersion -Description $Event.PackageName
             }
         }
-        If ($PackageSnapshot.Type -eq "Acceptance") {
-            $dateToCheck = Get-Date $($PackageSnapshot.ModifiedAt)
+        If ($Event.SnapshotStage -eq 'Acceptance') {
+            $CurrentPackage = Get-LiquitPackage -ID $Event.PackageID
+            $CurrentSnapshot = Get-LiquitPackageSnapshot -ID $Event.SnapshotID -Package $CurrentPackage
+            If ($Event.DateOfStage -ne $null) {
+                $dateToCheck = Get-Date $($Event.DateOfStage)
+            }
             $sevenDaysAgo = (Get-Date).AddDays(-$daysBeforeProduction)
-            if ($dateToCheck -lt $sevenDaysAgo) {
-                Publish-LiquitPackageSnapshot -Snapshot $PackageSnapshot -Stage Production -Name $PackageSnapshot.Name -Description $PackageSnapshot.Description
+            If ($dateToCheck -lt $sevenDaysAgo -or $Event.DateOfStage -eq $null) {
+                Publish-LiquitPackageSnapshot -Snapshot $CurrentSnapshot -Stage Acceptance -Name $Event.PackageVersion -Description $Event.PackageName
             }
         }
+
     }
 }
