28495951120abed3e3d5dd4aa7a5050c1cf21c26
John Yoakum <jyoakum2013@gmail.com>
2025-04-03 08:09:21 -0400

    Update Start-LiquitScheduledTasks.ps1

--- Start-LiquitScheduledTasks.ps1
+++ Start-LiquitScheduledTasks.ps1
@@ -1,4 +1,8 @@
-﻿$LiquitURI = 'https://zone.liquit.com' # Replace this with your zone
+if (-not (Get-Module -ListAvailable -Name Liquit.Server.PowerShell)) {
+    Install-Module -Name Liquit.Server.PowerShell -Scope CurrentUser -Force
+}
+
+$LiquitURI = 'https://zone.liquit.com' # Replace this with your zone
 $username = 'local\admin' # Replace this with a service account you have created for creating and accessing this information
 $password = 'PASSWORD' # Enter the password for that service Account
 $credentials = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username, (ConvertTo-SecureString -String $password -AsPlainText -Force)
