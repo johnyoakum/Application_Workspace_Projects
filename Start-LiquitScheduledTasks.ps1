277de5d0997980c0a3e6bc30a1fe6b3ca07a2c48
John Yoakum <jyoakum2013@gmail.com>
2025-04-03 04:07:33 -0800

    Add files via upload

---
+++ Start-LiquitScheduledTasks.ps1
@@ -0,0 +1,12 @@
+﻿$LiquitURI = 'https://john.liquit.com' # Replace this with your zone
+$username = 'local\admin' # Replace this with a service account you have created for creating and accessing this information
+$password = 'IsaiahMaddux@2014' # Enter the password for that service Account
+$credentials = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username, (ConvertTo-SecureString -String $password -AsPlainText -Force)
+$ScheduledTasks = "Synchronize Windows Apps","Synchronize Mac Apps"
+
+Connect-LiquitWorkspace -URI $LiquitURI -Credential $credentials
+
+ForEach ($ScheduledTask in $ScheduledTasks) {
+    $CurrentTask = Get-LiquitScheduleTask -Name "$ScheduledTask"
+    Start-LiquitScheduledTask -ScheduledTask $CurrentTask
+}
