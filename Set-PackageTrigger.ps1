476459cc4687ed7d8b17c72cd4bbc05e89f515c9
John Yoakum <jyoakum2013@gmail.com>
2025-04-03 04:06:52 -0800

    Add files via upload

---
+++ Set-PackageTrigger.ps1
@@ -0,0 +1,17 @@
+﻿Import-Module Liquit.Server.PowerShell
+$LiquitURI = 'https://john.liquit.com' # Replace this with your zone
+$username = 'local\admin' # Replace this with a service account you have created for creating and accessing this information
+$password = 'IsaiahMaddux@2014' # Enter the password for that service Account
+$credentials = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username, (ConvertTo-SecureString -String $password -AsPlainText -Force)
+$UpdatePackageNames = 'Update Audacity'
+
+Connect-LiquitWorkspace -URI $LiquitURI -Credential $credentials
+
+ForEach ($UpdatePackageName in $UpdatePackageNames) {
+    $UpdatePackage = Get-LiquitPackage -Name "$UpdatePackageName"
+    $CurrentEntitlement = Get-LiquitPackageEntitlement -Package $UpdatePackage | Where-Object {$_.ID -eq 'LOCAL\everyone'}
+    $PackageEntitlementEvent = New-Object Liquit.API.Server.V3.PackageEntitlementEvent
+    $PackageEntitlementEvent.Type = "Refresh"
+    $PackageEntitlementEvent.ActionSet = "Install"
+    Set-LiquitPackageEntitlement -Events @($PackageEntitlementEvent) -Entitlement $CurrentEntitlement
+}
