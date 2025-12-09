59f8819ee718dbff784850571505182fd7d97f5f
John Yoakum <jyoakum2013@gmail.com>
2025-12-09 12:23:29 -0600

    Add files via upload

---
+++ Install-FromCloud.ps1
@@ -0,0 +1,79 @@
+﻿param (
+  [String]$storageAccountName = "madduxliquit" ,           # Storage Account Name
+  [String]$containerName = "liquit" ,                          # Blob Container Name
+  [String]$url = "https://download.liquit.com/extra/Bootstrapper/AgentBootstrapper-Win-2.1.0.2.exe"
+)
+
+#https://madduxliquit.blob.core.windows.net/liquit/agent.json
+######################
+#   Configuration    #
+######################
+
+# Files to download
+$blobFiles = @(
+    "Agent.json",
+    "AgentRegistration.cer"
+)
+$DestinationPath = "C:\InstallFiles"               # Target path in the AIB VM
+$InstallerPath = "C:\InstallFiles\AgentBootstrapper.exe"
+$InstallerArguments = "/certificate=C:\InstallFiles\AgentRegistration.cer /startDeployment /waitForDeployment /logPath=C:\Windows\Temp"
+
+
+#######################################
+#    DOWNLOAD FILES TO DESTINATION    #
+#######################################
+# Create destination directory
+if (!(Test-Path $DestinationPath)) {  
+    New-Item -ItemType Directory -Path $DestinationPath -Force | Out-Null
+}
+
+# Download Agent Bootstrapper direct from Internet
+Invoke-WebRequest -Uri $url -OutFile $InstallerPath -UseBasicParsing
+
+foreach ($blobName in $blobFiles) {
+    $localFilePath = Join-Path $DestinationPath $blobName
+    $blobUrl = "https://$($storageAccountName).blob.core.windows.net/$($containerName)/$($blobName)"
+    #$blobUrl = "https://$storageAccountName.blob.core.windows.net/$containerName/$blobName"                
+    #Invoke-WebRequest -Uri $blobUrl -Headers $headers -OutFile $localFilePath
+    Invoke-WebRequest -Uri $blobUrl -OutFile $localFilePath
+
+    Write-Output "Downloading $blobName to $localFilePath..."
+    try {
+        Invoke-RestMethod -Uri $blobUrl -Headers $headers -OutFile $localFilePath
+        Write-Output "$blobName downloaded successfully."
+    } catch {
+        Write-Output "Failed to download $blobName $_"
+    }
+}
+
+Write-Output "All downloads completed."
+
+
+# ===============================
+set-location $DestinationPath
+
+# Start the install process
+
+Write-Host "Starting the installation process..."
+
+if (Test-Path -Path $InstallerPath) {
+
+   try {
+
+       Start-Process -FilePath $InstallerPath -ArgumentList $InstallerArguments -Wait
+
+       Write-Host "Installation process completed."
+
+   } catch {
+
+       Write-Error "Error starting the installer '$InstallerPath': $($_.Exception.Message)"
+
+       exit 1
+
+   }
+
+} else {
+
+   Write-Warning "Installer executable not found: '$InstallerPath'"
+
+}
